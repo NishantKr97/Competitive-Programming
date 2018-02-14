26afe9d47b45b2b8e0da43670f99beb354d49d0a
NishantKr97 <nishantkr1997@gmail.com>
2018-02-14 21:35:56 +0530

    6

---
+++ Hackerank/PlusMinus.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void plusMinus(vector <int> arr) {
+    // Complete this function
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       cin >> arr[arr_i];
+    }
+    plusMinus(arr);
+    return 0;
+}
