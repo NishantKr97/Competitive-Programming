453505e2bd4a1c9d6a701ab7928bb10965364dbe
NishantKr97 <nishantkr1997@gmail.com>
2018-03-19 00:59:24 +0530

    Kruskal's Algorithm

--- Design And Analysis of Algorithms/kruskal.cpp
+++ Design And Analysis of Algorithms/kruskal.cpp
@@ -1,63 +1,104 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
-{
-    public:
-        int value;
-        int rnk;
-        Node *parent;
+class node{
+public:
+	node* parent;
+	int value;
+	int rank;
+	node(int a){
+		this->value=a;
+	}
 
-        Node (int x)
-        {
-                this->value = x;
-        }
 };
 
 
-class Set
-{
-    public:
-        Node *root;
+class Set{
+public:
 
-        Node *make_set(int x)
-        {
-            Node *n = new Node(x);
-            n->rnk = 0;
-            n->value = x;
-            n->parent = n;
-        }
+	node* makeSet(int y){
+		node *x = new node (y);
+		x->parent= x;
+		x->rank=0;
+		return x;
+	}
 
-        Node *findSet(Node* x)
-        {
-            Node* temp = x;
-            while(x->parent != x)
-            {
-                x = x->parent;
-            }
-            temp->parent = x;
-            return x;
-        }
+	node* findSet(node* x){
+		if(x->parent==x)
+            return x ;
+        x->parent = findSet(x->parent);
+        return x->parent;
+	}
 
-        void Union(Node* x, Node* y)
-        {
-            Node* a = findSet(x);
-            Node* b = findSet(y);
-            if(a->rnk > b->rnk)
-                b->parent = a;
-            else if(b->rnk > a->rnk)
-                a->parent = b;
-            else
-            {
-                a->parent = b;
-                b->rnk++;
-            }
-        }
+	node* Union(node* x,node* y){
+		node* a=findSet(x);
+		node* b=findSet(y);
+		if(a->rank > b->rank){
+			b->parent = a;
+		}
+		else {
+			a->parent = b;
+			if(b->rank == a->rank)
+				b->rank++;
+		}
+
+	}
 };
+
+struct edge{
+    int u;
+    int v;
+    int w;
+};
+
+bool compareWeights(struct edge a,struct edge b){
+    return (a.w < b.w);
+}
 
 int main()
 {
-        int n,i,j,e;
+      int v,e,i,j;
+      cout<<"Enter the no. of nodes : ";
+      cin>>v;
+      Set s;
+      node* V[v];
+      for(i=0;i<v;i++)
+      {
+          V[i] = s.makeSet(i);
+      }
+      cout<<"Enter the no. of edges : ";
+      cin>>e;
+      vector<struct edge> E;
+      struct edge ed;
+      for(i=0;i<e;i++)
+      {
+          cin>>ed.u>>ed.v>>ed.v;
+          E.push_back(ed);
+      }
+      sort(E.begin(), E.end(), compareWeights);
+
+      vector<struct edge> T;
+      for(i=0;i<e;i++)
+      {
+            ed = E[i];
+            node* temp1 = s.findSet(V[ed.u]);
+            node* temp2 = s.findSet(V[ed.v]);
+            if(temp1 != temp2)
+            {
+                T.push_back(ed);
+                s.Union(V[ed.u], V[ed.v]);
+            }
+      }
+      cout<<endl;
+
+      for(i=0;i<T.size();i++){
+        cout<<T[i].u<<"\t"<<T[i].v<<"\t"<<T[i].w<<"\n";
+    }
+
+}
+
+/*
+  int n,i,j,e;
         cout<<"Enter the no. of Nodes : ";
         cin>>n;
         cout<<"Enter no. of edges : ";
@@ -86,6 +127,10 @@ int main()
         }
 
         sort(a.begin(), a.end());
+        for(i=0;i<a.size();i++)
+        {
+            cout<<a[i].first<<endl;
+        }
 
         for(i=0;i<e;i++)
         {
@@ -103,4 +148,4 @@ int main()
 
 
 
-}
+*/
