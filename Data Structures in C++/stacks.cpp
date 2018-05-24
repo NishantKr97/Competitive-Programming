826ae63d495c8f7558b30341d517f5626caa28f7
NishantKr97 <nishantkr1997@gmail.com>
2018-05-24 12:01:31 +0530

    Stack Operations

--- Data Structures in C++/stacks.cpp
+++ Data Structures in C++/stacks.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+
+void expEval()
 {
     stack<char> s, m;
     string exp = "[(])";
@@ -52,5 +53,73 @@ int main()
     else
         cout<<"False";
 
+}
 
+
+stack<int> createStack()
+{
+    stack<int> s;
+    s.push(5);
+    s.push(3);
+    s.push(9);
+    s.push(6);
+    s.push(10);
+
+    return s;
+}
+
+stack<int> insertAtBottom(stack<int> s, int item)
+{
+    if(s.empty())
+        s.push(item);
+    else
+    {
+            int temp = s.top();
+            s.pop();
+            s = insertAtBottom(s, item);
+
+            s.push(temp);
+    }
+    return s;
+}
+
+stack<int> reverseStack(stack<int> s)
+{
+    if(!s.empty())
+    {
+        int temp = s.top();
+        s.pop();
+        s = reverseStack(s);
+
+        s = insertAtBottom(s, temp);
+    }
+    return s;
+}
+
+void printStack(stack<int> s)
+{
+    if(s.empty())
+        return;
+    else{
+        cout<<s.top()<<" ";
+        s.pop();
+        return printStack(s);
+    }
+}
+
+int main()
+{
+        //expEval();
+
+        //createToReverse();
+
+        stack<int> s, r;
+        s = createStack();
+        cout<<"Original Stack : ";
+        printStack(s);
+        r = reverseStack(s);
+        cout<<"\nReversed Stack : ";
+        printStack(r);
+
+        //sortStack(s);
 }
